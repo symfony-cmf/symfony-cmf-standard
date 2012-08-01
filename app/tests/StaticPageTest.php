dc29ecde812f3db58de8feb716a8253b158f2480
lsmith77 <smith@pooteeweet.org>
2012-08-01 20:46:23 +0200

    added some hierarchical data

--- app/tests/StaticPageTest.php
+++ app/tests/StaticPageTest.php
@@ -15,14 +15,17 @@ class StaticPageTest extends WebTestCase
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
 
-        $this->assertCount(1, $crawler->filter(sprintf('h1:contains("%s")', $title)));
+        $this->assertCount(1, $crawler->filter(sprintf('h1:contains("%s")', $title)), 'Page does not contain an h1 tag with: '.$title);
     }
 
     public function contentDataProvider()
     {
         return array(
-            array('/service/about', 'Some information about us'),
-            array('/service/contact', 'A contact page'),
+            array('/', 'Welcome to the CMF Standard Edition'),
+            array('/about', 'Some information about us'),
+            array('/contact', 'A contact page'),
+            array('/contact/map', 'A map page'),
+            array('/contact/team', 'A team page'),
         );
     }
 }
--- src/Acme/MainBundle/DataFixtures/PHPCR/LoadSimpleCmsData.php
+++ src/Acme/MainBundle/DataFixtures/PHPCR/LoadSimpleCmsData.php
@@ -29,18 +29,30 @@ class LoadSimpleCmsData implements FixtureInterface, OrderedFixtureInterface, Co
 
     public function load(ObjectManager $manager)
     {
+        $session = $manager->getPhpcrSession();
+
         $basepath = $this->container->getParameter('symfony_cmf_simple_cms.basepath');
 
-        $session = $manager->getPhpcrSession();
-        if ($session->nodeExists("$basepath")) {
-            $session->removeItem("$basepath");
+        $basepath = explode('/', $basepath);
+        $rootname = array_pop($basepath);
+        $basepath = implode('/', $basepath);
+
+        if ('' === $basepath) {
+            $basepath = '/';
+        } else {
+            if ($session->nodeExists($basepath)) {
+                $session->removeItem($basepath);
+            }
         }
-        $this->createPath($session, $basepath);
+
+        $base = $this->createPath($session, $basepath);
         $base = $manager->find(null, $basepath);
 
-        $root = $this->createPage($manager, $base, 'service', 'root', 'root page of service menu, never used', '');
+        $root = $this->createPage($manager, $base, $rootname, 'homepage', 'Welcome to the CMF Standard Edition', 'This is should get you started with the Symfony CMF.');
         $this->createPage($manager, $root, 'about', 'About us', 'Some information about us', 'The about us page with some content');
-        $this->createPage($manager, $root, 'contact', 'Contact', 'A contact page', 'Please send an email to symfony-cmf-devs@groups.google.com');
+        $contact = $this->createPage($manager, $root, 'contact', 'Contact', 'A contact page', 'Please send an email to symfony-cmf-devs@groups.google.com');
+        $this->createPage($manager, $contact, 'map', 'Map', 'A map page', 'Have a look at the map to find us.');
+        $this->createPage($manager, $contact, 'team', 'Team', 'A team page', 'Our team consists of C, M and F.');
 
         $manager->flush();
     }
