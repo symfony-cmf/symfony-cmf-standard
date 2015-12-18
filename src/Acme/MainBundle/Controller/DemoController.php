ffcd6d800f00b2bc806010ebb66dad53a836813a
David Buchmann <david@liip.ch>
2015-12-18 03:31:09 -0500

    Applied fixes from StyleCI

--- src/Acme/MainBundle/Controller/DemoController.php
+++ src/Acme/MainBundle/Controller/DemoController.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Symfony CMF package.
+ *
+ * (c) 2011-2015 Symfony CMF
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Acme\MainBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
